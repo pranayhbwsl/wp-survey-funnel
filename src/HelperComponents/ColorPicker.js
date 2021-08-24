2acd58fb1e0b590ed323389fc3b73cdbada84ae5
Pranay <pranay.chahare@hbwsl.com>
2021-08-24 10:33:10 +0530

    Deign corrections

--- src/HelperComponents/ColorPicker.js
+++ src/HelperComponents/ColorPicker.js
@@ -3,27 +3,28 @@ import { RgbaColorPicker } from "react-colorful";
 
 import useClickOutside from "./useClickOutside";
 
-function rgbatohex(color){
+function rgbatohex(color) {
     var r = color.r;
     var g = color.g;
     var b = color.b;
     var a = color.a;
-    var outParts = [
-      r.toString(16),
-      g.toString(16),
-      b.toString(16),
-      Math.round(a * 255).toString(16).substring(0, 2)
-    ];
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+    a = Math.round(a * 255).toString(16);
   
-    // Pad single-digit output values
-    outParts.forEach(function (part, i) {
-      if (part.length === 1) {
-        outParts[i] = '0' + part;
-      }
-    })
-  
-    return ('#' + outParts.join(''));
-}
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+    if (a.length == 1)
+      a = "0" + a;
+    console.log("#" + r + g + b + a)
+    return "#" + r + g + b + a;
+  }
+
 
 export const PopoverPicker = ({ color, onChange }) => {
     const popover = useRef();
